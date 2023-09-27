7a7be3889fb6da175ef3a17dbcf08ab3d2863e67
M307users <m307user@m307.uc-bcf.edu.ph>
2023-09-27 13:22:28 +0800

    first commit po

---
+++ variables.php
@@ -0,0 +1,24 @@
+<h1><?php
+$intvar =  420;
+echo "This is integer: ";
+echo $intvar;
+$dvar =  10.1;
+
+echo "<br>";
+define("message", "This is constant: ");
+define ("nice", 69);
+echo message;
+echo nice; 
+
+echo "<br>";
+echo "This is integer with operations: ";
+$a =  $intvar + 1;
+echo $a;
+echo "<br>";
+echo "This is double: ";
+$b = $intvar + $dvar;
+echo $b;
+
+
+
+?></h1>
